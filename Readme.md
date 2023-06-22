8a3b73fd425f01702644972df29eda6ce41f620b
unknown <moreira.araujo99@gmail.com>
2023-06-21 22:55:29 -0300

    Desafio FrontendMentor

---
+++ Readme.md
@@ -0,0 +1,60 @@
+# Frontend Mentor - Age calculator app solution
+
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q).
+
+## Table of contents
+
+-   [Overview](#overview)
+    -   [The challenge](#the-challenge)
+    -   [Screenshot](#screenshot)
+    -   [Links](#links)
+-   [My process](#my-process)
+    -   [Built with](#built-with)
+    -   [Useful resources](#useful-resources)
+-   [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+-   View an age in years, months, and days after submitting a valid date through the form
+-   Receive validation errors if:
+    -   Any field is empty when the form is submitted
+    -   The day number is not between 1-31
+    -   The month number is not between 1-12
+    -   The year is in the future
+    -   The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+-   View the optimal layout for the interface depending on their device's screen size
+-   See hover and focus states for all interactive elements on the page
+-   **Bonus**: See the age numbers animate to their final number when the form is submitted
+
+### Screenshot
+
+![Desktop](./assets/Screenshots/design-desktop.png)
+![Mobile](./assets/Screenshots/design-mobile.png)
+![Empty Field Error](./assets/Screenshots/empty-field-error.png)
+![Invalid Day Error](./assets/Screenshots/invalid-day-error.png)
+![Invalid Field Error](./assets/Screenshots/invalid-field-error.png)
+
+### Links
+
+-   Solution URL: [Add solution URL here](https://your-solution-url.com)
+-   Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+-   HTML
+-   CSS
+-   Javascript
+
+### Useful resources
+
+-   (Chatgpt) - he helped me to debug code and help me to implement some.
+
+## Author
+
+-   Frontend Mentor - [@GabrielMA0](https://www.frontendmentor.io/profile/GabrielMA0)
---
+++ script.js
@@ -0,0 +1,184 @@
+const inputDayElement = document.querySelector(".input-day");
+const inputMonthElement = document.querySelector(".input-month");
+const inputYearElement = document.querySelector(".input-year");
+const resultDay = document.querySelector("#days");
+const resultMonth = document.querySelector("#months");
+const resultYear = document.querySelector("#years");
+const items = document.querySelector(".items");
+const labelDay = document.querySelector(".text-label-day");
+const labelMonth = document.querySelector(".text-labels-month");
+const labelYear = document.querySelector(".text-labels-year");
+const messageEmptyFieldDay = document.querySelector(".message-empty-field-day");
+const messageEmptyFieldMonth = document.querySelector(
+    ".message-empty-field-month"
+);
+const messageEmptyFieldYear = document.querySelector(
+    ".message-empty-field-year"
+);
+const messageErrorDay = document.querySelector(".message-error-day");
+const messageErrorMonth = document.querySelector(".message-error-month");
+const messageErrorYear = document.querySelector(".message-error-year");
+const btnCalculate = document.querySelector("#btn-calculate");
+let valid = false;
+
+// FUNÇÕES PARA BLOQUEAR LETRAS E CARACTERES ESPECIAIS
+inputDayElement.addEventListener("keypress", function (e) {
+    if (!checkChar(e)) {
+        e.preventDefault();
+    }
+});
+
+inputMonthElement.addEventListener("keypress", function (e) {
+    if (!checkChar(e)) {
+        e.preventDefault();
+    }
+});
+
+inputYearElement.addEventListener("keypress", function (e) {
+    if (!checkChar(e)) {
+        e.preventDefault();
+    }
+});
+
+document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        btnCalculate.click();
+    }
+});
+
+// FUNÇÃO PARA VERIFICAR OS CARACTERES
+function checkChar(e) {
+    var char = String.fromCharCode(e.keyCode);
+    var pattern = "[0-9]";
+    if (char.match(pattern)) {
+        return true;
+    }
+}
+
+function emptyInput(value) {
+    return value === "" || value === null;
+}
+function incorrectInput(value, maxValue) {
+    return value > maxValue;
+}
+
+function handleFieldValidation(
+    inputValue,
+    maxValue,
+    labelElement,
+    inputElement,
+    messageEmptyFieldElement,
+    messageErrorElement,
+    resultElement,
+    valueIfInvalid,
+    ageInDate
+) {
+    if (emptyInput(inputValue)) {
+        labelElement.classList.add("error");
+        inputElement.style.borderColor = "red";
+        messageEmptyFieldElement.classList.remove("hide");
+        messageErrorElement.classList.toggle("hide", emptyInput(inputValue));
+        resultElement.innerHTML = valueIfInvalid;
+    } else if (incorrectInput(inputValue, maxValue)) {
+        labelElement.classList.add("error");
+        inputElement.style.borderColor = "red";
+        messageEmptyFieldElement.classList.add("hide");
+        messageErrorElement.classList.remove("hide");
+        resultElement.innerHTML = valueIfInvalid;
+    } else {
+        labelElement.classList.remove("error");
+        inputElement.style.borderColor = "rgba(0, 0, 0, 0.12)";
+        messageEmptyFieldElement.classList.add("hide");
+        messageErrorElement.classList.add("hide");
+        resultElement.innerHTML = ageInDate;
+    }
+}
+
+function calculateAge() {
+    const date = new Date();
+    const currentDay = date.getDate();
+    const currentMonth = date.getMonth() + 1;
+    const currentYear = date.getFullYear();
+    const valueInputDays = inputDayElement.value;
+    const valueInputMonths = inputMonthElement.value;
+    const valueInputYears = inputYearElement.value;
+
+    // CONDIÇÕES TERNARIAS PARA OS NÚMEROS NÃO SAIREM NEGATIVOS
+    let ageInYears = currentYear - valueInputYears;
+    let ageInDay =
+        valueInputDays > currentDay
+            ? valueInputDays - currentDay
+            : currentDay - valueInputDays;
+    let ageInMonth =
+        valueInputMonths > currentMonth
+            ? valueInputMonths - currentMonth
+            : currentMonth - valueInputMonths;
+
+    const userDate = new Date(
+        valueInputYears,
+        valueInputMonths - 1,
+        valueInputDays
+    );
+
+    handleFieldValidation(
+        valueInputDays,
+        31,
+        labelDay,
+        inputDayElement,
+        messageEmptyFieldDay,
+        messageErrorDay,
+        resultDay,
+        "--",
+        ageInDay
+    );
+    handleFieldValidation(
+        valueInputMonths,
+        12,
+        labelMonth,
+        inputMonthElement,
+        messageEmptyFieldMonth,
+        messageErrorMonth,
+        resultMonth,
+        "--",
+        ageInMonth
+    );
+    handleFieldValidation(
+        valueInputYears,
+        currentYear,
+        labelYear,
+        inputYearElement,
+        messageEmptyFieldYear,
+        messageErrorYear,
+        resultYear,
+        "--",
+        ageInYears
+    );
+
+    function checkDays() {
+        if (parseInt(valueInputDays) != userDate.getDate()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (
+        parseInt(valueInputDays) === 31 ||
+        parseInt(valueInputDays) === 30 ||
+        parseInt(valueInputDays) === 28
+    ) {
+        if (checkDays(valueInputDays, userDate)) {
+            labelDay.classList.add("error");
+            inputDayElement.style.borderColor = "red";
+            messageErrorDay.classList.remove("hide");
+            messageEmptyFieldDay.classList.add("hide");
+            resultDay.innerHTML = "--";
+        } else {
+            labelDay.classList.remove("error");
+            inputDayElement.style.borderColor = "rgba(0, 0, 0, 0.12)";
+            messageErrorDay.classList.add("hide");
+            messageEmptyFieldDay.classList.add("hide");
+            resultDay.innerHTML = ageInDay;
+        }
+    }
+}
